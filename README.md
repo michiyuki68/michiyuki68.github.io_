5e47ef99370b80b953c97c1ba8d7c1b8da7695f9
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-12 17:10:29 +0900

    new java

---
+++ README.md
@@ -0,0 +1,7 @@
+# JS CODE
+
+JavaScriptのテストコードをまとめたやつ
+
+table-total-sumとかはどっかで使えそう。→key upの時に発火させた方がいいかも
+
+author: okutani
---
+++ list-data.js
@@ -0,0 +1,116 @@
+var list = [
+  {
+    "dir_name": "chart.js-test",
+    "files": [
+      "bar.html",
+      "doughnut.html",
+      "line.html",
+      "pie.html",
+      "radar.html"
+    ]
+  },
+  {
+    "dir_name": "check-num-only",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "countdown-timer",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "google-maps-api-test",
+    "files": [
+      "index.html",
+      "utf8.html"
+    ]
+  },
+  {
+    "dir_name": "highlight.js",
+    "files": [
+      "index.html",
+    ]
+  },
+  {
+    "dir_name": "particle-universe",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "show-ip",
+    "files": [
+      "webrtc.html"
+    ]
+  },
+  {
+    "dir_name": "sl.js",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "start-javascript",
+    "files": [
+      "numgame/index.html",
+      "omikuji/index.html",
+      "slotmachine/index.html",
+      "stoptime/index.html",
+      "stopwatch/index.html",
+      "timejs/index.html",
+      "warikandentaku/index.html"
+    ]
+  },
+  {
+    "dir_name": "kaomoji-javascript",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "stickybox",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "table-total-sum",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "vue.js",
+    "files": [
+      "class.html",
+      "counter.html",
+      "for.html",
+      "model.html",
+      "on.html",
+      "show-if.html"
+    ]
+  },
+  {
+    "dir_name": "webstorage",
+    "files": [
+      "memo.html",
+      "test.html"
+    ]
+  },
+  {
+    "dir_name": "wow-animate",
+    "files": [
+      "index.html"
+    ]
+  },
+  {
+    "dir_name": "yubinbango",
+    "files": [
+      "index.html",
+      "ajaxzip3.html"
+    ]
+  }
+]
