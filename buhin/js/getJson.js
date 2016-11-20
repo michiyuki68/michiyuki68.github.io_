897f916e0e558e730c60e402254258f706bb5034
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-20 20:14:02 +0900

    s

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -4,10 +4,9 @@ $(function() {
       ulObj = $("#headlines"),
       len = data.length;
 
-//    for(var i = 0; i < len; i++) {
-var i=0;
+    for(var i = 0; i < len; i++) {
       ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
-//    }
+    }
 
 //$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</p></h2>';
 
