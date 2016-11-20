cb32d63d794d32158d57598537adb052eb8de35f
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-20 23:34:13 +0900

    y

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -7,9 +7,9 @@ $(function() {
 //    for(var i = 0; i < len; i++) {
     for(var i = 0; i < 1; i++) {
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
-$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div>');
+$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div>' + '<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
 //	'<div class="loader"></div>';
-	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
+//	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
 //	$('<div class="meta meta--preview">');
 //		$('<img class="meta__avatar" src=' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + ' alt="author02" />');
 //		$('<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>');
