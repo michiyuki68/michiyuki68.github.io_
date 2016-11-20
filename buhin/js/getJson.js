8221fbf0293299aa9606c98125d92170129eeea1
Michiyuki Katayama <Michiyuki Katayama>
2016-11-20 19:55:12 +0900

    aa

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -5,10 +5,10 @@ $(function() {
       len = data.length;
 
 //    for(var i = 0; i < len; i++) {
-//      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
+      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
 //    }
 
-$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</p></h2>';
+//$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</p></h2>';
 
 
   });
