ac900f6f056bce56aedeac5ba96d0fe2afc06191
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-21 01:49:39 +0900

    gt

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -17,7 +17,7 @@ $(function() {
 //	$('</div>');
 //	$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '<div class="loader"></div>' + '<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>' + '<div class="meta meta--preview">' + '<img class="meta__avatar" src="' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + '" alt="author02" />' + '<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>' + '<span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].kijiLength)) + '</span>' + '</div>');
 
-	$('<a class="grid__item" href="#"><h2 class="title title--preview">' + ulObj.append($("<a>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '<div class="loader"></div>' + '<span class="category">' + ulObj.append($("<a>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span></a>');
+	$('<a class="grid__item" href="#"><h2 class="title title--preview">' + ulObj.append($("<a>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div>' + '<span class="category">' + ulObj.append($("<a>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span></a>');
 
 
 
