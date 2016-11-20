296fc08efc14b9a61ed1dfbef4ece1c9fb94ebca
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-20 19:21:57 +0900

    aaa

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -5,18 +5,14 @@ $(function() {
       len = data.length;
 
 //    for(var i = 0; i < len; i++) {
-      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
+//      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
 //    }
 
-	<h2 class="title title--preview"><p id="headlines"></p></h2>
-	<div class="loader"></div>
-	<span class="category">ˆê–Ê</span>
-	<div class="meta meta--preview">
-	<img class="meta__avatar" src="../buhin/img/authors/2.jpg" alt="author02" /> 
-	<span class="meta__date"><i class="fa fa-calendar-o"></i> 7 Apr</span>
-	<span class="meta__reading-time"><i class="fa fa-clock-o"></i> •¶š”‚ğƒJƒEƒ“ƒg‚·‚é‚È‚Ç</span>
-	</div>
+$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</p></h2>';
+
+
 
 
   });
 });
+45tr
