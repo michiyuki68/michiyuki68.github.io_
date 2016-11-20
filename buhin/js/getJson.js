a538bbb0109ea8720ea11c95aab512adfb36eaf8
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-20 21:12:56 +0900

    j

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -8,14 +8,14 @@ $(function() {
     for(var i = 0; i < 1; i++) {
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
 $('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</p></h2>';
-	$('<div class="loader"></div>');
-	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
-	$('<div class="meta meta--preview">');
-		$('<img class="meta__avatar" src=' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + ' alt="author02" />');
-		$('<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>');
-		$('<span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].kijiLength)) + '</span>');
-	$('</div>');
-
+//	$('<div class="loader"></div>');
+//	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
+//	$('<div class="meta meta--preview">');
+//		$('<img class="meta__avatar" src=' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + ' alt="author02" />');
+//		$('<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>');
+//		$('<span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].kijiLength)) + '</span>');
+//	$('</div>');
+	$('<div class="loader"></div>' + '<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>' + '<div class="meta meta--preview">'+ '<img class="meta__avatar" src=' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + ' alt="author02" />' + '<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>' + '<span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].kijiLength)) + '</span>' + '</div>');
 
 
     }
