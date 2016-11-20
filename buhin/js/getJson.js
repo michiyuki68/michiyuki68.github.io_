841d7068f5b47a5e1504d0bdd829d44f192a854e
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-20 23:30:53 +0900

    yy

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -9,7 +9,7 @@ $(function() {
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
 $('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div>');
 //	'<div class="loader"></div>';
-//	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
+	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
 //	$('<div class="meta meta--preview">');
 //		$('<img class="meta__avatar" src=' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + ' alt="author02" />');
 //		$('<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>');
