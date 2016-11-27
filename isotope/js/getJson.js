b6863f441a79abbdd0abe8f4216e321ae48ca753
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-28 00:10:31 +0900

    r

--- isotope/js/getJson.js
+++ isotope/js/getJson.js
@@ -11,7 +11,7 @@ $(function() {
 //    for(var i = 0; i < len; i++) {
 
     for(var i = 0; i < 1; i++) {
-
+ulObj.after("""<div class=""element-item transition metal click"" data-category=""transition""><p class=""articleSubCategory"">‚«‚å‚¤‚Ì‚±‚Æ‚Î</p>""");
         ulObj.append($("<p>").attr("class", "articleCategory").text(data[i].category));
         ulObj.append($("<p>").attr("class", "headline").text(data[i].HeadLine));
 //        ulObj.append($("<p>").attr("class", "firstArticle overflow").text(data[i].FirstBody));
