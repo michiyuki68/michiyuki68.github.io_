7019682d2463840ba114637e88982e47e12d908d
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-27 20:35:28 +0900

    u

--- isotope/js/getJson.js
+++ isotope/js/getJson.js
@@ -6,8 +6,8 @@ $(function() {
 
     for(var i = 0; i < len; i++) {
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].name));
-      ulObj.append($("<p class="headline">").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
-      ulObj.append($("<p class="firstArticle overflow>").attr({"id":data[i].seqNo}).text(data[i].FirstBody));
+      ulObj.append($("<p>").attr({"id":data[i].seqNo}, "headline").text(data[i].HeadLine));
+      ulObj.append($("<p>").attr({"id":data[i].seqNo}, "firstArticle overflow").text(data[i].FirstBody));
     }
   });
 });
