3b885d78ad3fe78b86702ddaf5e22cdd4e4b289a
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-27 21:06:50 +0900

    s

--- isotope/js/getJson.js
+++ isotope/js/getJson.js
@@ -2,6 +2,7 @@ $(function() {
   $.getJSON("data.json" , function(data) {
     var
       ulObj = $("#demo"),
+      appObj= ulObj.append($("<p>"),
       len = data.length;
 
 //    for(var i = 0; i < len; i++) {
@@ -9,10 +10,10 @@ $(function() {
     for(var i = 0; i < 1; i++) {
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].name));
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}, "headline").text(data[i].HeadLine));
-        ulObj.append($("<p>").attr("class", "articleCategory").text(data[i].category));
-        ulObj.append($("<p>").attr("class", "headline").text(data[i].HeadLine));
+        appObj.attr("class", "articleCategory").text(data[i].category));
+        appObj.attr("class", "headline").text(data[i].HeadLine));
 //        ulObj.append($("<p>").attr("class", "firstArticle overflow").text(data[i].FirstBody));
-        ulObj.append($("<p>").attr("class", "firstArticle").text(data[i].FirstBody));
+        appObj.attr("class", "firstArticle").text(data[i].FirstBody));
     }
   });
 });
