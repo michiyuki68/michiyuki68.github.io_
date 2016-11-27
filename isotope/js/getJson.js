2a4903db0ad965f6a1f4e486b34722b0b397763f
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-27 21:54:59 +0900

    s

--- isotope/js/getJson.js
+++ isotope/js/getJson.js
@@ -8,10 +8,10 @@ $(function() {
 //    for(var i = 0; i < len; i++) {
 
     for(var i = 0; i < 1; i++) {
-        ulObj.append((appObj).attr("class", "articleCategory").text(data[i].category));
+        ulObj.append($("<p>").attr("class", "articleCategory").text(data[i].category));
         ulObj.append($("<p>").attr("class", "headline").text(data[i].HeadLine));
 //        ulObj.append($("<p>").attr("class", "firstArticle overflow").text(data[i].FirstBody));
-        appObj.attr("class", "firstArticle").text(data[i].FirstBody));
+        ulObj.append($("<p>").attr("class", "firstArticle").text(data[i].FirstBody));
     }
   });
 });
