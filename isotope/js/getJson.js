047ec0b6ef29fd6cc74fb6e4f96d965d5139699d
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-27 21:26:15 +0900

    d

--- isotope/js/getJson.js
+++ isotope/js/getJson.js
@@ -2,14 +2,12 @@ $(function() {
   $.getJSON("data.json" , function(data) {
     var
       ulObj = $("#demo"),
-      appObj= ulObj.append($("<p>"),
+      appObj= ulObj.append($("<div>"),
       len = data.length;
 
 //    for(var i = 0; i < len; i++) {
 
     for(var i = 0; i < 1; i++) {
-//      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].name));
-//      ulObj.append($("<p>").attr({"id":data[i].seqNo}, "headline").text(data[i].HeadLine));
         appObj.attr("class", "articleCategory").text(data[i].category));
         appObj.attr("class", "headline").text(data[i].HeadLine));
 //        ulObj.append($("<p>").attr("class", "firstArticle overflow").text(data[i].FirstBody));
