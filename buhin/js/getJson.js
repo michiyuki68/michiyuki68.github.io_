60702bb0121b845ec636ae191f544558ecaa5a20
michiyuki68 <michiyuki.k68@gmail.com>
2016-11-30 00:52:22 +0900

    s

--- buhin/js/getJson.js
+++ buhin/js/getJson.js
@@ -6,18 +6,7 @@ $(function() {
 
 //    for(var i = 0; i < len; i++) {
     for(var i = 0; i < 1; i++) {
-//      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine));
-//$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div>' + '<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
-//	'<div class="loader"></div>';
-//	$('<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>');
-//	$('<div class="meta meta--preview">');
-//		$('<img class="meta__avatar" src=' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + ' alt="author02" />');
-//		$('<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>');
-//		$('<span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].kijiLength)) + '</span>');
-//	$('</div>');
-//	$('<h2 class="title title--preview">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '<div class="loader"></div>' + '<span class="category">' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].category)) + '</span>' + '<div class="meta meta--preview">' + '<img class="meta__avatar" src="' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].img)) + '" alt="author02" />' + '<span class="meta__date"><i class="fa fa-calendar-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].date)) + '</span>' + '<span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].kijiLength)) + '</span>' + '</div>');
-
-	$('<a class="grid__item" href="#"><h2 class="title title--preview">' + ulObj.append($("<a>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div></a>');
+	$('<a class="grid__item transition" href="#" data-category="transition"><h2 class="title title--preview">' + ulObj.append($("<a>").attr({"id":data[i].seqNo}).text(data[i].HeadLine)) + '</h2><div class="loader"></div><span class="category">ˆê–Ê</span><div class="meta meta--preview"><span class="meta__date"><i class="fa fa-calendar-o"></i> 7 Apr</span><span class="meta__reading-time"><i class="fa fa-clock-o"></i> •¶š”‚ğƒJƒEƒ“ƒg‚·‚é‚È‚Ç</span></div></a>');
 
 
 
