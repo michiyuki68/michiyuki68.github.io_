4ff1fcd88c9fc87267236dab3c1e275c159c0790
michiyuki68 <michiyuki.k68@gmail.com>
2016-12-03 11:54:03 +0900

    j

--- 20161130/js/getJson.js
+++ 20161130/js/getJson.js
@@ -4,12 +4,12 @@ $(function() {
       ulObj = $("#michiyuki"),
       len = data.length;
 
-    for(var i = 0; i < 1; i++) {
+    for(var i = 0; i < len; i++) {
 //      ulObj.append($("<p>").attr({"id":data[i].seqNo}).text(data[i].name));
 //      ulObj.append($("<p>" + data[i].HeadLine + "</p>"));
 
 ulObj.after($('<a class="grid__item transition" href="#" data-category="transition"><h2 class="title title--preview">' + data[i].HeadLine + '</h2><div class="loader"></div><span class="category">' + data[i].category + '</span><div class="meta meta--preview"><img class="meta__avatar" src="img/authors/1.png" alt="author01"/><span class="meta__date"><i class="fa fa-calendar-o"></i> 9 Apr</span><span class="meta__reading-time"><i class="fa fa-clock-o"></i> 3 min read</span></div></a>'));
-    
+
     }
   );
 }});
