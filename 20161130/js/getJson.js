b335d23d93c302192a38a0299d25e96b01f60cf6
michiyuki68 <michiyuki.k68@gmail.com>
2016-12-12 15:39:29 +0900

    a

--- 20161130/js/getJson.js
+++ 20161130/js/getJson.js
@@ -10,10 +10,12 @@ console.log('data.jsonとおってるよ');
 //console.log('data.json０とおってるよ');
 //        ulObj1.append($('<a class="grid__item ' + data[i].Category + '" href="#" data-category="' + data[i].category + '"><h2 class="title title--preview">' + data[i].Headline + '</h2><div class="loader"></div><span class="category">' + data[i].Category + '</span><div class="meta meta--preview"><img class="meta__avatar" src="img/authors/1.png" alt="author01"/><span class="meta__date"><i class="fa fa-calendar-o"></i>' + document.lastModified + '</span><span class="meta__reading-time"><i class="fa fa-clock-o"></i> 3 min read</span></div></a>'));
 
-console.log(data[i].FirstBody.substr(0, 150));
-var firstBodyPreview;
-if (data[i].FirstBody.length > 150){
-  firstBodyPreview = data[i].FirstBody.substr(0, 150) + '…';
+
+var firstBodyPreview,
+    limitNo;
+limitNo = 100;
+if (data[i].FirstBody.length > limitNo){
+  firstBodyPreview = data[i].FirstBody.substr(0, limitNo) + '…';
 }
 
         ulObj1.append($('<a class="grid__item ' + data[i].Category + '" href="#" data-category="' + data[i].category + '"><h2 class="title title--preview">' + data[i].Headline + '</h2><div class="loader"></div><div class="meta">' + firstBodyPreview + '</div>'));
