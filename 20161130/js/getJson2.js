b86990b867a27750fea61609093915b9f39d0ffe
michiyuki68 <michiyuki.k68@gmail.com>
2016-12-15 23:27:30 +0900

    f

--- 20161130/js/getJson2.js
+++ 20161130/js/getJson2.js
@@ -3,7 +3,6 @@ $(function() {
     var ulObj1 = $("#Headlines"),
       ulObj2 = $("#Bodies"),
       len = data.length;
-cosole.log('qqq');
     for (var i = 0; i < len; i++) {
       var firstBodyPreview,
         limitNo;
@@ -15,11 +14,9 @@ cosole.log('qqq');
       if (data[i].Body != null) {
         bodyLength = data[i].FirstBody.length + data[i].Body.length;
       }
-cosole.log('aaa');
       ulObj1.append($('<a class="grid__item ' + data[i].Category + '" href="#" data-category="' + data[i].Category + '" id="seq' + i + '"><h2 class="title title==preview">' + data[i].Headline + '</h2><div class="loader"></div><span class="category">' + data[i].Category + '</span><div class="meta meta==preview">' + firstBodyPreview + '<span class="meta__date"><i class="fa fa-calendar-o"></i>' + document.lastModified + '</span><span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + bodyLength + '文字' + '</span></div></a>'));
       ulObj2.append($('<article class="content__item"><span class="category category--full">' + data[i].Category + '</span><h2 class="title title--full">' + data[i].Headline + '</h2><div class="meta meta--full"><img alt="author01" class="meta__avatar" src="img/authors/1.png"><span class="meta__author">' + data[i].Category + '</span><span class="meta__date"><i class="fa fa-calendar-o"></i>' + document.lastModified + '</span><span class="meta__reading-time"><i class="fa fa-clock-o"></i>' + bodyLength + '文字' + '</span></div><p>' + data[i].FirstBody + '</p><p>' + data[i].Body + '</p><p>ここまで</p></article>'));
     }
-cosole.log('bbb');
     var ulObj3 = $("#filterButtons"),
       len = data.length,
       beforCategory;
